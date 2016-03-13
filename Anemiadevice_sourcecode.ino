4eef9155c13338a05f2942a1fb0ab2999c44bb24
AdiuvoDiagnostics <bioe.geethanjali@gmail.com>
2016-03-13 17:26:06 +0530

    Added files via upload
    Anemia is a major pubic health problem affecting more than 500 million of the indian population and leading to 58 percent of maternal deaths in India. Currently many devices are available but needs a prick of blood and dedicated instruments to calculate the hemoglobin content accurately. Here we propose a non-invasive technique to periodically monitor hemoglobin content and can especially  be used to screen anemia on a regular basis in pregnant woman and children using pulse plethysmography. The device needs a single point calibration with zero subsequent screening cost. In addition device is portable and can be used in rural settings as a personal monitoring kit or can be used by a health worker.
    
    This non-invasive device works on the principle of pulse plethysmography.
    We have analysed the transmitted IR signal and found the standard deviation for 300 intervals, which is then calibrated against a known anemia detection technique. This one step calibration is then extrapolated in s

---
+++ Anemiadevice_sourcecode.ino
@@ -0,0 +1,376 @@
+//#include <b64.h>
+#include <Average.h>
+#include <HttpClient.h>
+#include <LTask.h>
+#include <LWiFi.h>
+#include <LWiFiClient.h>
+#include <LDateTime.h>
+#define WIFI_AP "Gee"
+#define WIFI_PASSWORD "abcd1234"
+#define WIFI_AUTH LWIFI_WPA  // choose from LWIFI_OPEN, LWIFI_WPA, or LWIFI_WEP.
+#define per 50
+#define per1 1
+#define DEVICEID "D0LofDZo" // Input your deviceId
+#define DEVICEKEY "GWCIjfxu5lB5VMaz" // Input your deviceKey
+#define SITE_URL "api.mediatek.com"
+int npoints=200;
+Average<float> ave(npoints);
+int i;
+
+LWiFiClient c;
+unsigned int rtc;
+unsigned int lrtc;
+unsigned int rtc1;
+unsigned int lrtc1;
+char port[4]={0};
+char connection_info[21]={0};
+char ip[21]={0};             
+int portnum;
+int val = 0;
+String tcpdata = String(DEVICEID) + "," + String(DEVICEKEY) + ",0";
+String upload_led;
+String tcpcmd_led_on = "Device_Control,1";
+String tcpcmd_led_off = "Device_Control,0";
+
+LWiFiClient c2;
+HttpClient http(c2);
+
+void setup()
+{
+  
+  LTask.begin();
+  LWiFi.begin();
+  Serial.begin(115200);
+  while(!Serial) delay(1000); /* comment out this line when Serial is not present, ie. run this demo without connect to PC */
+
+  Serial.println("Connecting to AP");
+  while (0 == LWiFi.connect(WIFI_AP, LWiFiLoginInfo(WIFI_AUTH, WIFI_PASSWORD)))
+  {
+    delay(1000);
+  }
+  
+  Serial.println("calling connection");
+
+  while (!c2.connect(SITE_URL, 80))
+  {
+    Serial.println("Re-Connecting to WebSite");
+    delay(1000);
+  }
+  delay(100);
+
+  pinMode(18, OUTPUT);
+  Serial.println("I am in set-up loop");
+  getconnectInfo();
+  connectTCP();
+}
+
+void getconnectInfo(){
+  //calling RESTful API to get TCP socket connection
+  c2.print("GET /mcs/v2/devices/");
+  c2.print(DEVICEID);
+  c2.println("/connections.csv HTTP/1.1");
+  c2.print("Host: ");
+  c2.println(SITE_URL);
+  c2.print("deviceKey: ");
+  c2.println(DEVICEKEY);
+  c2.println("Connection: close");
+  c2.println();
+  
+  delay(500);
+
+  int errorcount = 0;
+  while (!c2.available())
+  {
+    Serial.println("waiting HTTP response: ");
+    Serial.println(errorcount);
+    errorcount += 1;
+    if (errorcount > 10) {
+      c2.stop();
+      return;
+    }
+    delay(100);
+  }
+  int err = http.skipResponseHeaders();
+
+  int bodyLen = http.contentLength();
+  Serial.print("Content length is: ");
+  Serial.println(bodyLen);
+  Serial.println();
+  char c;
+  int ipcount = 0;
+  int count = 0;
+  int separater = 0;
+  while (c2)
+  {
+    int v = c2.read();
+    if (v != -1)
+    {
+      c = v;
+      Serial.print(c);
+      connection_info[ipcount]=c;
+      if(c==',')
+      separater=ipcount;
+      ipcount++;    
+    }
+    else
+    {
+      Serial.println("no more content, disconnect");
+      c2.stop();
+
+    }
+    
+  }
+  Serial.print("The connection info: ");
+  Serial.println(connection_info);
+  int i;
+  for(i=0;i<separater;i++)
+  {  ip[i]=connection_info[i];
+  }
+  int j=0;
+  separater++;
+  for(i=separater;i<21 && j<5;i++)
+  {  port[j]=connection_info[i];
+     j++;
+  }
+  Serial.println("The TCP Socket connection instructions:");
+  Serial.print("IP: ");
+  Serial.println(ip);
+  Serial.print("Port: ");
+  Serial.println(port);
+  portnum = atoi (port);
+  Serial.println(portnum);
+  Serial.println("Ended getting the connection information");
+
+} //getconnectInfo
+
+void uploadstatus(){
+  //calling RESTful API to upload datapoint to MCS to report LED status
+  Serial.println("calling connection");
+  LWiFiClient c2;  
+
+  while (!c2.connect(SITE_URL, 80))
+  {
+    Serial.println("Re-Connecting to WebSite");
+    delay(1000);
+  }
+  delay(100);
+ if(digitalRead(18)==1)
+  upload_led = "Device_Status,,1";
+ else
+  upload_led = "Device_Status,,0";
+  int thislength = upload_led.length();
+  HttpClient http(c2);
+  c2.print("POST /mcs/v2/devices/");
+  c2.print(DEVICEID);
+  c2.println("/datapoints.csv HTTP/1.1");
+  c2.print("Host: ");
+  c2.println(SITE_URL);
+  c2.print("deviceKey: ");
+  c2.println(DEVICEKEY);
+  c2.print("Content-Length: ");
+  c2.println(thislength);
+  c2.println("Content-Type: text/csv");
+  c2.println("Connection: close");
+  c2.println();
+  c2.println(upload_led);
+
+  //upload_led = "PPG_Sensor,,14";
+  
+  delay(500);
+
+  int errorcount = 0;
+  while (!c2.available())
+  {
+    Serial.print("waiting HTTP response: ");
+    Serial.println(errorcount);
+    errorcount += 1;
+    if (errorcount > 10) {
+      c2.stop();
+      return;
+    }
+    delay(100);
+  }
+  int err = http.skipResponseHeaders();
+
+  int bodyLen = http.contentLength();
+  Serial.print("Content length is: ");
+  Serial.println(bodyLen);
+  Serial.println();
+  while (c2)
+  {
+    int v = c2.read();
+    if (v != -1)
+    {
+      Serial.print(char(v));
+    }
+    else
+    {
+      Serial.println("no more content, disconnect");
+      c2.stop();
+
+    }
+    
+  }
+}
+
+void uploadstatus2(){
+  //calling RESTful API to upload datapoint to MCS to report LED status
+  
+  int minat = 0;
+  int maxat = 0;
+  Serial.println("calling connection 2");
+  LWiFiClient c2;  
+
+  while (!c2.connect(SITE_URL, 80))
+  {
+    Serial.println("Re-Connecting to WebSite");
+    delay(1000);
+  }
+  //delay(100);
+ //if(digitalRead(13)==1)
+ //int sensorValue = analogRead(A0);
+  //upload_led = "PPG_Sensor,,"+String(sensorValue);
+  float sensorValue;
+  for (i=0;i<npoints;i++)
+  {
+          sensorValue = (float)analogRead(A0)*5/1024;
+          //float ppgvalue=sensorValue*5/1024;
+          ave.push(sensorValue);
+          Serial.println(ave.get(i));
+          delay(1);
+  }
+  Serial.print("Mean:   "); Serial.println(ave.mean());
+  Serial.print("Max:    "); Serial.println(ave.maximum(&maxat));
+  Serial.print("Min:    "); Serial.println(ave.minimum(&minat));
+  Serial.print("StdDev: "); Serial.println(ave.stddev());
+  // print out the value you read:
+  //Serial.println("Printing Sensor Value");
+  
+  delay(10);        // delay in between reads for stability
+  float ppgvalue=ave.stddev()*1000;
+  //Serial.println(ave.stddev);
+  //Serial.println(sensorValue);
+  upload_led = "PPG_Sensor,,"+String(int(ppgvalue));
+  int thislength = upload_led.length();
+  HttpClient http(c2);
+  c2.print("POST /mcs/v2/devices/");
+  c2.print(DEVICEID);
+  c2.println("/datapoints.csv HTTP/1.1");
+  c2.print("Host: ");
+  c2.println(SITE_URL);
+  c2.print("deviceKey: ");
+  c2.println(DEVICEKEY);
+  c2.print("Content-Length: ");
+  c2.println(thislength);
+  c2.println("Content-Type: text/csv");
+  c2.println("Connection: close");
+  c2.println();
+  c2.println(upload_led);
+
+  //upload_led = "PPG_Sensor,,14";
+  
+  delay(5);
+
+  int errorcount = 0;
+  while (!c2.available())
+  {
+    Serial.print("waiting HTTP response: ");
+    Serial.println(errorcount);
+    errorcount += 1;
+    if (errorcount > 10) {
+      c2.stop();
+      return;
+    }
+    delay(10);
+  }
+  int err = http.skipResponseHeaders();
+
+  int bodyLen = http.contentLength();
+  Serial.print("Content length is: ");
+  Serial.println(bodyLen);
+  Serial.println();
+  while (c2)
+  {
+    int v = c2.read();
+    if (v != -1)
+    {
+      Serial.print(char(v));
+    }
+    else
+    {
+      Serial.println("no more content, disconnect");
+      c2.stop();
+
+    }
+    
+  }
+}
+
+void connectTCP(){
+  //establish TCP connection with TCP Server with designate IP and Port
+  c.stop();
+  Serial.println("Connecting to TCP");
+  Serial.println(ip);
+  Serial.println(portnum);
+  while (0 == c.connect(ip, portnum))
+  {
+    Serial.println("Re-Connecting to TCP");    
+    delay(100);
+  }  
+  Serial.println("send TCP connect");
+  c.println(tcpdata);
+  c.println();
+  Serial.println("waiting TCP response:");
+}//connectTCP
+
+void heartBeat(){
+  Serial.println("send TCP heartBeat");
+  c.println(tcpdata);
+  c.println();
+    
+} //heartBeat
+
+void loop()
+{
+  //Check for TCP socket command from MCS Server 
+  //Serial.println("I am in main loop");
+
+  String tcpcmd="";
+  while (c.available())
+   {
+      int v = c.read();
+      
+      if (v != -1)
+      {
+        Serial.print((char)v);
+        //Serial.println("I am in inside loop");
+        tcpcmd += (char)v;
+        if (tcpcmd.substring(40).equals(tcpcmd_led_on)){
+          digitalWrite(18, HIGH);
+          Serial.print("Switch LED ON ");
+          tcpcmd="";
+        }else if(tcpcmd.substring(40).equals(tcpcmd_led_off)){  
+          digitalWrite(18, LOW);
+          Serial.print("Switch LED OFF");
+          tcpcmd="";
+        }
+      }
+   }
+
+  
+  LDateTime.getRtc(&rtc);
+  if ((rtc - lrtc) >= per) {
+    heartBeat();
+    lrtc = rtc;
+  }
+  //Check for report datapoint status interval
+  LDateTime.getRtc(&rtc1);
+  if ((rtc1 - lrtc1) >= per1) {
+    uploadstatus();
+    uploadstatus2();
+    lrtc1 = rtc1;
+  }
+  
+}
+
+
